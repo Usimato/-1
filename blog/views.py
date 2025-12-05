baa463efd873132c5becd535a4547d3beb53cb26
Usimato <Kratosjiv@mail.ru>
2025-12-05 17:40:41 +0700

    feat(posts): Добавил функционал получения деталей поста: - шаблон с деталями поста - маршрут и обработчик для этого маршрута

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 from blog.models import Post  
+
+
 def get_post_list(request):
     posts = Post.objects.all()
-    return render(
-        request=request, template_name='blog/post_list.html', context={'posts': posts}  
-    )
+    
+    return render(request=request, template_name='blog/post_list.html', context={'posts': posts})
+    
+    
+def get_post_detail(request, post_id):
+    return render(request, "blog/post_detail.html", {"post": get_object_or_404(Post, id=post_id)})
 
 
 
