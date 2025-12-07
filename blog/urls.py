4a2daef7544d341773cef00d4abc03495ee7325b
Usimato <Kratosjiv@mail.ru>
2025-12-07 17:55:42 +0700

    feat(nav): Добавил навигационное меню

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path("posts/", views.get_post_list, name='post_list'),
-path("posts/<int:post_id>/", views.get_post_detail, name='post_detail'),
-path('posts/add/', views.create_post),
+    path("posts/", views.get_post_list, name='post_list'),
+    path("posts/<int:post_id>/", views.get_post_detail, name='post_detail'),
+    path("posts/add/", views.create_post, name='new_post'),
 ]
 
