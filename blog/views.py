07751200192cdf19c934149b574d1786c3003d8f
Usimato <Kratosjiv@mail.ru>
2025-12-08 23:37:33 +0700

    feat(posts): Добавил обработку ошибок в форму создания поста: -Отображение ошибок в шаблоне -Сделал так чтобы значения полей не сбрасывались при повторном рендере

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,41 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from blog.models import Post
 
+
 def get_post_list(request):
     posts = Post.objects.all()
-    return render(request=request, template_name='blog/post_list.html', context={'posts': posts})
+    return render(request, template_name='blog/post_list.html', context={'posts': posts})
+
 
 def get_post_detail(request, post_id):
-    return render(request, 'blog/post_detail.html', {'post': get_object_or_404(Post, id=post_id)})
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def create_post(request):
     if request.method == "GET":
         return render(request, 'blog/post_add.html')
 
     if request.method == "POST":
-        post = Post.objects.create(
-            title=request.POST.get("title"),
-            text=request.POST.get("text")
-        )
-        return redirect('post_detail', post_id=post.id)   
+        title = request.POST.get('title', '').strip()
+        text = request.POST.get('text', '').strip()
+        errors = {}
+
+        if not title:
+            errors['title'] = 'Заголовок обязателен.'
+        if not text:
+            errors['text'] = 'Текст поста обязательно нужно указать.'
+
+        if errors:
+            context = {
+                'errors': errors,
+                'title': title,
+                'text': text
+            }
+            return render(request, 'blog/post_add.html', context)
+
+        post = Post.objects.create(title=title, text=text)
+        return redirect('post_detail', post_id=post.id)
     
+
+
