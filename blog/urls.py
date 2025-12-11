45e4fd34c6ed23020e1725dab1d09993e4b3d051
Usimato <Kratosjiv@mail.ru>
2025-12-12 00:45:26 +0700

    feat(posts): Добавил функционал для изменения поста

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("posts/", views.get_post_list, name='post_list'),
     path("posts/<int:post_id>/", views.get_post_detail, name='post_detail'),
     path("posts/add/", views.create_post, name='new_post'),
+    path("posts/<int:post_id>/edit/", views.update_post),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -17,14 +17,33 @@ def get_post_detail(request, post_id):
 
 def create_post(request):
     form = PostForm(request.POST or None)
-  
+
     if request.method == "POST":
         if form.is_valid():
             post = form.save()
+
             return redirect('post_detail', post_id=post.id)
-        # Если форма невалидна, продолжим к render ниже
-  
+            # Если форма невалидна, продолжим к render ниже
+
     return render(request, 'blog/post_add.html', {"form": form})
+
+
+def update_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect("post_detail", post_id=post.id)
+        else:
+            return render(request, 'blog/post_update.html', context={"form": form})
+
+    form = PostForm(instance=post)
+
+    return render(request, 'blog/post_update.html', context={"form": form})
 
 
 
