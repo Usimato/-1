987e5d7bb9f93f9497eda67f525554991d441df4
Usimato <Kratosjiv@mail.ru>
2025-12-11 15:47:43 +0700

    refactor(posts): Оптимизировал вальюшку создания поста (forms.Form)

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import render, get_object_or_404, redirect
 
 from blog.models import Post
@@ -12,13 +11,14 @@ def get_post_list(request):
 
 
 def get_post_detail(request, post_id):
+    # return render(request, 'blog/post_detail.html', {"post": Post.objects.get(id=post_id)})
     return render(request, 'blog/post_detail.html', {"post": get_object_or_404(Post, id=post_id)})
 
 
 def create_post(request):
+    form = PostForm(request.POST or None)
+  
     if request.method == "POST":
-        form = PostForm(request.POST)
-
         if form.is_valid():
             post = Post.objects.create(
                 title=form.cleaned_data['title'],
@@ -26,12 +26,8 @@ def create_post(request):
             )
 
             return redirect('post_detail', post_id=post.id)
-        else:
-            return render(request, 'blog/post_add.html', {"form": form})
-
-    # GET
-    form = PostForm()
-
+        # Если форма невалидна, продолжим к render ниже
+  
     return render(request, 'blog/post_add.html', {"form": form})
 
 
