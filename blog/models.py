7d4a0322251a40c5556ec7561c78658a206652f6
Usimato <Kratosjiv@mail.ru>
2025-12-27 19:30:07 +0700

    feat(posts/image): Установил pillow, добавил картинку для поста, добавил отображение картинки в деталях поста.

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,9 @@ User = get_user_model()
 class Post(models.Model):
     title = models.CharField(max_length=200, verbose_name="Заголовок")
     text = models.TextField(verbose_name="Текст")
+    image = models.ImageField(upload_to="post_images/", null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts') # можно указать SET_NULL
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')  # можно указать SET_NULL
 
     class Meta:
         verbose_name = 'пост'
--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_bootstrap5',
+    'django_bootstrap_icons',
     'django_extensions',
     'blog',
     'users'
@@ -120,6 +121,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static/']
+
+
+# Media Files (User files)
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
