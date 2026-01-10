24c2a236ca9db2baca633796091eaa98e0539c87
Usimato <Kratosjiv@mail.ru>
2026-01-10 14:38:00 +0700

    feat(posts/tags): Ручная обработка тегов: изменил связанный с моделью tags в PostForm на несвязанный с моделью tags

--- blog/forms.py
+++ blog/forms.py
@@ -4,27 +4,34 @@ from blog.models import Post
 
 
 class PostForm(forms.ModelForm):
+    tags_input = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введите теги через запятую'
+        }),
+        label="Теги"
+    )
+
     class Meta:
         model = Post
-        fields = ['title', 'category', 'tags', 'text', 'image']
+        fields = ['title', 'category', 'text', 'image']
         widgets = {
-        'title': forms.TextInput(attrs={
-            'placeholder': "заголовок (максимальная длина 200 символов)"
-        }),
-        'category': forms.Select(attrs={'class': 'form-control'}),
-        'tags': forms.SelectMultiple(attrs={'class': 'form-control'}),
-        'image': forms.FileInput(attrs={'class': 'form-control'})
+            'title': forms.TextInput(attrs={
+                'placeholder': "заголовок (максимальная длина 200 символов)"
+            }),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'image': forms.FileInput(attrs={'class': 'form-control'})
         }
-    labels = {
-        'title': 'Заголовок поста:',
-        'category': 'Категория:',
-        'tags': 'Теги:',
-        'text': 'Текст поста:',
-        'image': 'Картинка поста'
+        labels = {
+            'title': 'Заголовок поста:',
+            'category': 'Категория:',
+            'text': 'Текст поста:',
+            'image': 'Картинка поста'
         }
-    help_texts = {
-        'category': "- можно выбрать только одну категорию"
-    }
+        help_texts = {
+            'category': "- можно выбрать только одну категорию"
+        }
 
     def clean_title(self):
         title = self.cleaned_data['title'].strip()
@@ -37,8 +44,15 @@ class PostForm(forms.ModelForm):
         
         return title
 
-
-
+    def clean_tags_input(self):
+        """
+        Разбивает строку на список тегов:
+        - удаляет лишние пробелы вокруг
+        - приводит к нижнему регистру
+        """
+        tags_str = self.cleaned_data.get('tags_input', '')  # ← если None, будет пустая строка
+        tags = [tag.strip().lower() for tag in tags_str.split(',') if tag.strip()]
+        return tags
 
 
 
--- blog/views.py
+++ blog/views.py
@@ -53,10 +53,14 @@ def create_post(request):
             post.slug = slugify(unidecode(post.title))
             post.save()
 
-            form.save_m2m()
+            tags = form.cleaned_data.get('tags_input')
+
+            for tag_name in tags:
+                tag, _ = Tag.objects.get_or_create(name=tag_name)
+                post.tags.add(tag)
 
             return redirect('blog:post_detail', post_slug=post.slug)
-    # Если форма невалидна, продолжим к render ниже
+        # Если форма невалидна, продолжим к render ниже
 
     return render(request, 'blog/pages/post_form.html', {"form": form, 'title': title, 'submit_button_text': submit_button_text})
 
@@ -76,11 +80,18 @@ def update_post(request, post_id):
         if form.is_valid():
             form.save()
 
+            tags = form.cleaned_data.get('tags_input')
+            post.tags.clear()
+            for tag_name in tags:
+                tag, _ = Tag.objects.get_or_create(name=tag_name)
+                post.tags.add(tag)
+
             return redirect("blog:post_detail", post_slug=post.slug)
         else:
             return render(request, 'blog/pages/post_form.html', context={"form": form, 'title': title, 'submit_button_text': submit_button_text})
 
-    form = PostForm(instance=post)
+    existing_tags = ", ".join(tag.name for tag in post.tags.all())
+    form = PostForm(instance=post, initial={'tags_input': existing_tags})
 
     return render(request, 'blog/pages/post_form.html', context={"form": form, 'title': title, 'submit_button_text': submit_button_text})
 
