2534977353cdafd51fe6cf998bd9382d3ea4e358
Usimato <Kratosjiv@mail.ru>
2026-01-16 00:06:22 +0700

    refactor(users/views.py): Переписал register_view на CBV

--- users/views.py
+++ users/views.py
@@ -1,23 +1,18 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, get_user_model
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
 from django.conf import settings
-from blog.models import Post
 
 User = get_user_model()
 
 
-def register_view(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('users:login')
-        else:
-            return render(request, 'users/pages/register.html', {'form': form})
-    form = UserCreationForm()
-    
-    return render(request, 'users/pages/register.html', {'form': form})
+class RegisterView(CreateView):
+    template_name = 'users/pages/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('users:login')
 
 
 def login_view(request):
@@ -25,16 +20,17 @@ def login_view(request):
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             login(request, form.get_user())
-            
-            next_url = request.GET.get('next', settings.DEFAULT_LOGIN_REDIRECT_URL)
+
+            next_url = request.GET.get('next', settings.DEFAULT_LOGIN_REDIRECT_URL) # next будет '/posts/add/', например
             if next_url == settings.DEFAULT_LOGIN_REDIRECT_URL:
                 return redirect(next_url, request.user.username)
             else:
                 return redirect(next_url)
         else:
             return render(request, 'users/pages/login.html', {'form': form})
-    
+
     form = AuthenticationForm()
+
     return render(request, 'users/pages/login.html', {'form': form})
 
 
@@ -46,10 +42,10 @@ def logout_view(request):
 def profile_view(request, username):
     user = get_object_or_404(User, username=username)
     posts = user.posts.order_by('-created_at')
-    
+
     context = {
         'user': user,
         'posts': posts
     }
-    
+
     return render(request, 'users/pages/profile.html', context)
