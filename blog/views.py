0d88da59f79224c4d9a44c99b8250e1ae0c36ca6
Usimato <Kratosjiv@mail.ru>
2026-01-23 00:53:08 +0700

    feat(posts/search): Добавил пагинацию на страницу post_search (учёл GET-параметры). Неважное: Отключил подсказки и проверку орфографии для поисковика

--- blog/views.py
+++ blog/views.py
@@ -18,6 +18,7 @@ class PostListView(ListView):
 class PostSearchView(ListView):
     template_name = "blog/pages/post_search.html"
     context_object_name = 'posts'
+    paginate_by = 3
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
